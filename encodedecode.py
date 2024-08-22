88380f354fa96da0a46f807b45bf7f10833297bc
Avinash Nagaram <avinash.nagaram@coxautoinc.com>
2024-08-21 22:23:31 -0600

    question updated

--- encodedecode.py
+++ encodedecode.py
@@ -1,6 +1,31 @@
+"""
+
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+Please implement encode and decode
+
+Example 1:
+
+Input: ["neet","code","love","you"]
+
+Output:["neet","code","love","you"]
+Example 2:
+
+Input: ["we","say",":","yes"]
+
+Output: ["we","say",":","yes"]
+Constraints:
+
+0 <= strs.length < 100
+0 <= strs[i].length < 200
+strs[i] contains only UTF-8 characters.
+
+"""
+
 #python> this is the medium level of the problem
 #["my","name","is","sai"] -> "2#my3#name2#is3#sai"  encode
 #"2#my3#name2#is3#sai" -> ["my","name","is","sai"]  decode
+
 
 class encodedecode:
     def encode(self,l):
