f37776d012165545d91e4b88a384f184fd63d386
Avinash Nagaram <avinash.nagaram@coxautoinc.com>
2024-08-21 22:21:13 -0600

    encode decode implementaion

--- encodedecode.py
+++ encodedecode.py
@@ -1,1 +1,31 @@
-#python
+#python> this is the medium level of the problem
+#["my","name","is","sai"] -> "2#my3#name2#is3#sai"  encode
+#"2#my3#name2#is3#sai" -> ["my","name","is","sai"]  decode
+
+class encodedecode:
+    def encode(self,l):
+        res = ""
+        for i in l:
+            res += str(len(i)) + "#" + i
+        return res
+
+    def decode(self,s):
+        res = []
+        i = 0 
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length =  int(s[i:j])
+            res.append(s[j+1:j+length+1])
+            i = j + length + 1
+        return res
+
+
+obj = encodedecode()
+encoded = obj.encode(["my","name","is","sai"])
+print(encoded)
+
+decoded = obj.decode(encoded)
+print(decoded)
